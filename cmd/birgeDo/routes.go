32bec1c785ff0d3f17def6be256dc0267ec16604
jumagaliev1 <200103311@stu.sdu.edu.kz>
2023-01-10 23:20:45 +0600

    :egg: Composable Middleware Chains

--- cmd/birgeDo/routes.go
+++ cmd/birgeDo/routes.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/julienschmidt/httprouter"
+	"github.com/justinas/alice"
+	_ "github.com/justinas/alice"
 	"net/http"
 )
 
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
-	//fileServer := http.FileServer(http.Dir("./ui/static/"))
+	standardMiddleware := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 	router.HandlerFunc(http.MethodGet, "/", app.home)
 	router.HandlerFunc(http.MethodGet, "/room", app.createRoom)
 	router.HandlerFunc(http.MethodPost, "/room", app.createRoom)
@@ -16,5 +18,5 @@ func (app *application) routes() http.Handler {
 
 	//router.Handler(http.MethodGet, "/static/", http.StripPrefix("/static", fileServer))
 	router.ServeFiles("/static/*filepath", http.Dir("ui/static"))
-	return app.recoverPanic(app.logRequest(secureHeaders(router)))
+	return standardMiddleware.Then(router)
 }
--- go.mod
+++ go.mod
@@ -7,4 +7,7 @@ require (
 	golang.org/x/crypto v0.5.0
 )
 
-require github.com/lib/pq v1.10.7
+require (
+	github.com/justinas/alice v1.2.0
+	github.com/lib/pq v1.10.7
+)

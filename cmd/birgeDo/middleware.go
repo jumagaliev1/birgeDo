6f639f82126ffe23c31b40813ffb6d053aea1cf8
jumagaliev1 <200103311@stu.sdu.edu.kz>
2023-02-23 18:35:55 +0600

    Fix cors cred true

--- cmd/birgeDo/middleware.go
+++ cmd/birgeDo/middleware.go
@@ -132,8 +132,9 @@ func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Origin")
 		w.Header().Add("Vary", "Access-Control-Request-Method")
-		origin := r.Header.Get("Origin")
-		w.Header().Set("Access-Control-Allow-Origin", origin)
+		//origin := r.Header.Get("Origin")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
 			w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
 			w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
